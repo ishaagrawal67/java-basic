8ecfe9d18f63cb12f4ea483320a46238183eca86
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2021-08-20 22:04:57 +0530

    area of hexagon

---
+++ area_of_hexagon.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class area_of_hexagon {
+    public static void main(String[] args){
+        int a=3;
+        double area;
+        area = (((3*1.73)/2)*a*a);
+        System.out.println("area of hexagon : "+area);
+
+
+    }
+}
