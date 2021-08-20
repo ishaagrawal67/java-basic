aaf7f1d8191790dd518a98cee2311facb233a4f2
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2021-08-20 21:41:27 +0530

    swap number

---
+++ SwapTwoNumber.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class SwapTwoNumber {
+    public static void main(String[] args){
+     int a=10,b=5,c;
+     c=a;
+     a=b;
+     b=c;
+     System.out.println("A = "+a);
+     System.out.println("B = "+b);
+    }
+}
