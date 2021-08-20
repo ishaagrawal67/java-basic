890e0847918ff65f127b930ed874bc608cb7660a
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2021-08-20 21:57:06 +0530

    percentage

---
+++ CalculatePercentage.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CalculatePercentage {
+    public static void main(String[] args){
+        int mark=100,total_mark=500,percentage;
+        percentage=(mark*100)/total_mark;
+        System.out.println("total percentage:"+percentage);
+
+    }
+}
