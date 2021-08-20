7e7437ea58b2b77cea623411789012d2f4a06f43
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2021-08-20 21:15:22 +0530

    area of circle

---
+++ test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class test{
+    public static void main(String[] args){
+        int r=4;
+        double area;
+        area=3.14*(r*r);
+        System.out.println("area of the circle:"+area);
+    }
+}
