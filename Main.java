6fafe2778f858cc8833f7b371d59f26fc186ff4f
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2021-08-20 21:30:35 +0530

    perimeyer of the circle

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        int r=2;
+        double perimeter;
+        perimeter=2*3.14*r;
+        System.out.println("perimeter of the circle:"+perimeter);
+    }
+}
