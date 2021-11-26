4322a84a5e25751955c57f5388a130380aa2e5cc
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2021-11-26 20:18:33 +0530

    parse int

---
+++ wrapperclass05.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class wrapperclass05{
+    public static void main(String[] args) {
+        String s=new String("10");
+        String a= new String("20");
+        int sum=Integer.parseInt(s)+Integer.parseInt(a);
+        System.out.println(sum);
+    }
+}
