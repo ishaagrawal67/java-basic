cd18476b05b9d3b0bc9256f0ee3d95b3500e6108
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2022-03-13 19:07:45 +0530

    buble string sort

---
+++ bubbleSort_02.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class bubbleSort_02 {
+    public static void main(String[] args) {
+        String[] a={"babli","eve","cati","amit","dev"};
+        String temp;
+        for(int i=0;i<a.length;i++)
+        {
+            int flag=0;
+            for(int j=0;j<a.length-1-i;j++)
+            {
+                if(a[j].compareTo(a[j+1])>0)
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    flag=1;
+                }
+            }
+            if (flag == 0){
+                break;
+            }
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
