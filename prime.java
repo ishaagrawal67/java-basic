30f9259f86162956b635af60784a730210ab495e
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2022-03-23 21:41:10 +0530

    prime number by recursion

--- prime.java
+++ prime.java
@@ -1,22 +1,24 @@
 package com.company;
 import java.util.Scanner;
 public class prime {
+    static int prime(int i,int n)
+    {
+        if(n==i)
+            return 0;
+        else
+            if(n%i==0)
+                return 1;
+            else
+                return prime(i+1,n);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int i, n, c=1;
-        n= sc.nextInt();
-        for (i=2;i<=n;i++){
-            if (n%i==0){
-                c+=1;
-            }
-
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++)
+        {
+            if(prime(2,i)==0)
+                System.out.println(i+" ");
         }
-        if (c==2){
-            System.out.println("prime");
-        }
-        else {
-            System.out.println("not prime");
-        }
-
     }
 }
