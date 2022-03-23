ba38054fb3a2c63f21523eac81c55cfa1e278bbc
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2022-03-23 21:39:30 +0530

    factorial  y using recursion

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class factorial {
+    static int factorial(int n)
+    {
+        if(n==0)
+            return 1;
+        return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
